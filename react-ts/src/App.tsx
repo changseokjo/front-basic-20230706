306217ff3def63847d3b73d391c6c54bdedc36f7
조창석 <yulini1022@gmail.com>
2023-08-18 14:32:27 +0900

    study-20230818

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Typescript from './Typescript';
+import Component from './components/Component';
+import State from './components/State';
 
 function App() {
+
+  const flag = 1;
+
   return (
-    <Typescript />
+    <div>
+      <State />
+      {/* {mock.map(item => (<Component title={item.title} content={item.content} />))}
+      { true && (<h1>true</h1>)}
+      { true ? (<h1>true</h1>) : (<h1>false</h1>) } */}
+    </div>
   );
 }
 
 export default App;
+
+const mock = [
+  {
+    title: '1 번째 제목',
+    content: '안녕하세요 반갑습니다.'
+  },
+  {
+    title: '2 번째 제목',
+    content: '안녕하세요 반갑습니다.'
+  },
+  {
+    title: '3 번째 제목',
+    content: '안녕하세요 반갑습니다.'
+  }
+];
