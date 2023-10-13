940b07923acae3d670629ecb62c455a217d05e9c
changseokjo <yulini1022@gmail.com>
2023-10-13 11:49:17 +0900

    2023.10.13.11:50 nest study

--- nest/realtime-back/src/chat/chat/chat.ts
+++ nest/realtime-back/src/chat/chat/chat.ts
@@ -1,11 +1,37 @@
-import { Injectable } from '@nestjs/common';
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+interface MessageDto {
+    room: string;
+    nickname: string;
+    message: string;
+}
 
 @WebSocketGateway(4020, { transports: ['websocket', 'polling'], cors: { origin: '*' } })
 export class Chat {
 
     @WebSocketServer()
     server: Server;
+    logger = new Logger();
 
+    @SubscribeMessage('join')
+    handleJoin(
+        @MessageBody() room: string,
+        @ConnectedSocket() Socket: Socket
+    ): void {
+        this.logger.warn(`Join Room : ${room}`);
+        Socket.join(room);
+    }
+
+    @SubscribeMessage('send')
+    handleSend(
+        @MessageBody() messageDto: MessageDto
+    ): void {
+        const { room, message, nickname } = messageDto;
+        this.logger.verbose(message);
+        this.server.to(room).emit('get', { nickname, message });
+    }
+
+    
 }
